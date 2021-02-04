7fe36ba51045eb93c20bb964ae7b201d3d10a3d6
Stjepan <dzojicsjepan1812@gmail.com>
2021-02-04 04:12:12 +0100

    Adding an Add button witn no functionality yet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from './Button'
 
 const Header = ({title}) => {
     return (
-        <header>
+        <header className="header">
             <h1>{title}</h1>
+            <Button text="Add"/>
         </header>
     )
 }
