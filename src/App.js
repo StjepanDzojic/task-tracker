22110c5681d4f6a3a1f8c098501bcc5171a92dc4
Stjepan <dzojicsjepan1812@gmail.com>
2021-02-04 04:38:47 +0100

    Adding tasks from a static array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import Header from './components/Header'
+import {useState} from 'react'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appintment",
+      day: "Feb 5th",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Shoping",
+      day: "Feb 7th",
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: "Meeting",
+      day: "Feb 6th",
+      reminder: true,
+    },
+  ])
+
   return (
     <div className="container">
-      <Header title="Task tracker"/>
+      <Header title="Task tracker" />
+      <Tasks tasks={tasks} />
     </div>
-  ); 
+  );
 }
 
 export default App;
