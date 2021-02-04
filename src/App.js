a7a2c26a714d687cb8bd41eaf69f89cc2949c931
Stjepan <dzojicsjepan1812@gmail.com>
2021-02-04 18:32:48 +0100

    Adding an AddTask file with inputs and checkbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Header from './components/Header'
-import {useState} from 'react'
-import Tasks from './components/Tasks'
+import Header from "./components/Header";
+import { useState } from "react";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask"
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -22,21 +23,27 @@ function App() {
       day: "Feb 6th",
       reminder: true,
     },
-  ])
+  ]);
+
 
   //Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter(task => task.id !== id ))
-  } 
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   const toggleReminder = (id) => {
-    setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task))
-  } 
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header title="Task tracker" />
-      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      <AddTask />
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
 
-const Header = ({title}) => {
-    return (
-        <header className="header">
-            <h1>{title}</h1>
-            <Button text="Add"/>
-        </header>
-    )
-}
+const Header = ({ title }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button text="Add" />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
  
